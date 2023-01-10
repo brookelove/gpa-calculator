a1fa1af8430d6f79eaf635e59f1a5b9d96f54f65
brookelove <brookelove@Brookes-iMac-2.local>
2023-01-09 19:10:21 -0800

    adding a neew row function begins

--- assets/script.js
+++ assets/script.js
@@ -2,35 +2,51 @@
 console.log("linked");
 let main = document.getElementById("main");
 let table = document.getElementById("inputGrades");
+let addRow = document.getElementById("addRow");
 
 // let firstQ = () => {
 //   const newH1 = document.createElement("h1");
 //   newH1.textContent = "How many grades do you "
 // };
 // firstQ();
-
+// want to create possibly an even to askj for amount of grades you think will be there
+let askAmountButton = () => {
+  let askAmount = document.createElement("h1");
+  askAmount.innerHTML = "What class is this for?";
+  let askText = document.createElement("input");
+  let askButton = document.createElement("button");
+};
 let createGradeInput = () => {
   for (i = 0; i < 7; i++) {
     // console.log(i);
     let newRow = document.createElement("tr");
 
+    let delBttnBox = document.createElement("td");
     let assignmentBox = document.createElement("td");
     let gradeBox = document.createElement("td");
     let weightBox = document.createElement("td");
+    let delBttn = document.createElement("button");
 
     let assignment = document.createElement("input");
     let grade = document.createElement("input");
     let weight = document.createElement("input");
 
+    delBttn.innerText = "Clear";
     assignmentBox.appendChild(assignment);
     gradeBox.appendChild(grade);
     weightBox.appendChild(weight);
+    delBttnBox.appendChild(delBttn);
 
     newRow.appendChild(assignmentBox);
     newRow.appendChild(gradeBox);
     newRow.appendChild(weightBox);
+    newRow.appendChild(delBttnBox);
 
     table.appendChild(newRow);
   }
 };
 createGradeInput();
+let addNewRow = () => {
+  console.log("clicked");
+};
+addRow.addEventListener("click", addNewRow);
