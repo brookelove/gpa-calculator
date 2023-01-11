444395b6105dea086b826de56bfb370a2c067722
brookelove <brookelove@Brookes-iMac-2.local>
2023-01-10 21:23:45 -0800

    reconfigure page to have inputs rather than a grid

--- assets/script.js
+++ assets/script.js
@@ -1,79 +1,70 @@
 // import "animate.css";
 console.log("linked");
 let main = document.getElementById("main");
-let table = document.getElementById("inputGrades");
-let addRow = document.getElementById("addRow");
+// let table = document.getElementById("gradeTable");
+let tableBody = document.getElementById("inputGrades");
+let addRowBTN = document.getElementById("addRow");
+let calculateBTN = document.getElementById("calculateGrade");
+let assignmentInput = document.getElementById("assignment");
+let gradeInput = document.getElementById("grade");
+let weightInput = document.getElementById("weight");
+let gradeArr = [];
 
-// let firstQ = () => {
-//   const newH1 = document.createElement("h1");
-//   newH1.textContent = "How many grades do you "
-// };
-// firstQ();
-// want to create possibly an even to askj for amount of grades you think will be there
 let askAmountButton = () => {
   let askAmount = document.createElement("h1");
   askAmount.innerHTML = "What class is this for?";
   let askText = document.createElement("input");
   let askButton = document.createElement("button");
 };
+// console.log(document.getElementById("gradeTable").rows[1].cells[0].innerHTML);
 // pass in the parameter of user input
-let createGradeInput = () => {
-  // make i equal to user input
-  for (i = 0; i < 7; i++) {
-    // console.log(i);
-    let newRow = document.createElement("tr");
-
-    let delBttnBox = document.createElement("td");
-    let assignmentBox = document.createElement("td");
-    let gradeBox = document.createElement("td");
-    let weightBox = document.createElement("td");
-    let delBttn = document.createElement("button");
-
-    let assignment = document.createElement("input");
-    let grade = document.createElement("input");
-    let weight = document.createElement("input");
-
-    delBttn.innerText = "Clear";
-    assignmentBox.appendChild(assignment);
-    gradeBox.appendChild(grade);
-    weightBox.appendChild(weight);
-    delBttnBox.appendChild(delBttn);
-
-    newRow.appendChild(assignmentBox);
-    newRow.appendChild(gradeBox);
-    newRow.appendChild(weightBox);
-    newRow.appendChild(delBttnBox);
-
-    table.appendChild(newRow);
-  }
+let calculateMyGrade = () => {
+  console.log(gradeArr);
 };
-createGradeInput();
+// calculateMyGrade();
+// createGradeInput();
 let addNewRow = () => {
-  console.log("clicked");
+  //   console.log("clicked");
   //   change the user input to be equal to one and then pass in i as 1
+  console.log(assignmentInput.value);
+  console.log(gradeInput.value);
+  console.log(weightInput.value);
+  let newGrade = {
+    assignment: assignmentInput.value,
+    grade: gradeInput.value,
+    weight: weightInput.value,
+  };
+  gradeArr.push(newGrade);
+
   let newRow = document.createElement("tr");
 
   let delBttnBox = document.createElement("td");
+  let editBttnBox = document.createElement("td");
   let assignmentBox = document.createElement("td");
   let gradeBox = document.createElement("td");
   let weightBox = document.createElement("td");
   let delBttn = document.createElement("button");
+  let editBttn = document.createElement("button");
 
-  let assignment = document.createElement("input");
-  let grade = document.createElement("input");
-  let weight = document.createElement("input");
-
+  assignmentBox.innerText = assignmentInput.value;
+  gradeBox.innerText = gradeInput.value;
+  weightBox.innerText = weightInput.value;
   delBttn.innerText = "Clear";
-  assignmentBox.appendChild(assignment);
-  gradeBox.appendChild(grade);
-  weightBox.appendChild(weight);
+  editBttn.innerText = "Edit";
+
   delBttnBox.appendChild(delBttn);
+  editBttnBox.appendChild(editBttn);
 
-  newRow.appendChild(assignmentBox);
-  newRow.appendChild(gradeBox);
-  newRow.appendChild(weightBox);
-  newRow.appendChild(delBttnBox);
+  newRow.append(assignmentBox, gradeBox, weightBox, delBttn, editBttn);
+  //   newRow.appendChild(gradeBox);
+  //   newRow.appendChild(weightBox);
+  //   newRow.appendChild(delBttnBox);
+  //   newRow.appendChild(editBttnBox);
 
-  table.appendChild(newRow);
+  tableBody.appendChild(newRow);
+  assignmentInput.value = "";
+  gradeInput.value = "";
+  weightInput.value = "";
 };
-addRow.addEventListener("click", addNewRow);
+addRowBTN.addEventListener("click", addNewRow);
+calculateBTN.addEventListener("click", calculateMyGrade);
