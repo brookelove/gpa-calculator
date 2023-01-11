45e770b81a47f7cda8255a2b64b854ea094c20c6
brookelove <brookelove@Brookes-iMac-2.local>
2023-01-11 12:13:06 -0800

    averaging grades based on code and pseudocoding results

--- assets/script.js
+++ assets/script.js
@@ -10,25 +10,39 @@ let gradeInput = document.getElementById("grade");
 let weightInput = document.getElementById("weight");
 let gradeArr = [];
 
-let askAmountButton = () => {
-  let askAmount = document.createElement("h1");
-  askAmount.innerHTML = "What class is this for?";
-  let askText = document.createElement("input");
-  let askButton = document.createElement("button");
-};
+// let askAmountButton = () => {
+//   let askAmount = document.createElement("h1");
+//   askAmount.innerHTML = "What class is this for?";
+//   let askText = document.createElement("input");
+//   let askButton = document.createElement("button");
+// };
 // console.log(document.getElementById("gradeTable").rows[1].cells[0].innerHTML);
 // pass in the parameter of user input
 let calculateMyGrade = () => {
-  console.log(gradeArr);
+  //   console.log(gradeArr.length);
+  let weightedArr = [];
+  let start = 0;
+  for (let i = 0; i < gradeArr.length; i++) {
+    // console.log(gradeArr[i].assignment);
+    // console.log(typeof gradeArr[i].weight);
+    // console.log(typeof gradeArr[i].grade);
+    let weightNum = parseFloat(gradeArr[i].weight) / 100;
+    console.log(weightNum);
+    let weightedGrade = weightNum * parseFloat(gradeArr[i].grade);
+    console.log(weightedGrade);
+    weightedArr.push(weightedGrade);
+    // console.log(weighedArr);
+  }
+  console.log(weightedArr);
+  //   console.log(weighedArr.reduce((a, b) => a + b));
+  weightedArr.forEach((item) => {
+    start += item;
+  });
+  console.log(start);
 };
 // calculateMyGrade();
 // createGradeInput();
 let addNewRow = () => {
-  //   console.log("clicked");
-  //   change the user input to be equal to one and then pass in i as 1
-  console.log(assignmentInput.value);
-  console.log(gradeInput.value);
-  console.log(weightInput.value);
   let newGrade = {
     assignment: assignmentInput.value,
     grade: gradeInput.value,
